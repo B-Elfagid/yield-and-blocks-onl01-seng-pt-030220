bdfc2709a42724e8ed404d084053af201268e0c4
Betelhem Elfagid <betty_elfagid@yahoo.co.uk>
2020-03-13 13:47:08 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -2,14 +2,13 @@ a = ["Tim", "Tom", "Jim"]
 
 def hello_t(array)
   i = 0
+ 
   while i < array.length
-  yield(array[i])
-  i = i + 1 
-  
-end
-
-array
-
+    yield(array[i])
+    i = i + 1
+  end
+ 
+  array
 end
 
 
